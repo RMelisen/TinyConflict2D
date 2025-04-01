48eee0c7e0c06636bea7f026f27861f99090754f
Romain M. <r.melisen@outlook.com>
2025-04-01 10:44:31 +0200

    Added "Unit" parent class. All units will inherit from this one

--- Units/Scripts/InfantryUnit.cs
+++ Units/Scripts/InfantryUnit.cs
@@ -1,6 +1,7 @@
 using Godot;
 using System;
 
-public partial class InfantryUnit : Node2D
+public partial class InfantryUnit : Unit
 {
+    
 }
---
+++ Units/Scripts/Unit.cs
@@ -0,0 +1,21 @@
+using Godot;
+using System;
+
+public partial class Unit : CharacterBody2D
+{
+    [Export]
+    public int Health { get; set; } = 100;
+
+    [Export]
+    public int MovementRange { get; set; } = 3;
+
+    public virtual void Move(Vector2 direction)
+    {
+        
+    }
+
+    public virtual void Attack(Unit target)
+    {
+        
+    }
+}
