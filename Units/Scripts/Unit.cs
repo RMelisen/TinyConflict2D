4ed4e162f7cb61ac01f53f99d2e3e6371bbd9429
Romain M. <r.melisen@outlook.com>
2025-04-01 10:50:52 +0200

    Refactoring

--- Units/Scripts/Unit.cs
+++ Units/Scripts/Unit.cs
@@ -3,19 +3,19 @@ using System;
 
 public partial class Unit : CharacterBody2D
 {
-    [Export]
-    public int Health { get; set; } = 100;
+	[Export]
+	public int Health { get; set; } = 100;
 
-    [Export]
-    public int MovementRange { get; set; } = 3;
+	[Export]
+	public int MovementRange { get; set; } = 3;
 
-    public virtual void Move(Vector2 direction)
-    {
-        
-    }
+	public virtual void Move(Vector2 direction)
+	{
+		
+	}
 
-    public virtual void Attack(Unit target)
-    {
-        
-    }
+	public virtual void Attack(Unit target)
+	{
+		
+	}
 }
